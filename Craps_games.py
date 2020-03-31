317f63d8cd6f112edee05921df3976fcc67e07f0
matheusrb <matheusbarrosr@gmail.com>
2020-03-31 10:51:08 -0300

    Mais comentaeios

--- Craps_games.py
+++ Craps_games.py
@@ -17,3 +17,23 @@
 #"if" a soma for 2, 3 ou 12 (Craps) o jogador perde a aposta.
 #"if" a soma for 4, 5, 6, 8, 9 ou 10 o jogo muda para a fase de 'Point'.
 #A aposta continua valendo, o valor tirado se torna o 'Point', para ganhar a soma do novo lancamento deve ser igual ao do point.
+#"if" a soma dos dados der 7 o jogador perde tudo.
+#"if" qualquer outro numero continua rodando o dado
+
+
+
+#Field - Pode ser feita em qualquer fase do jogo.
+#"if" a soma dos dados derem 5, 6, 7 ou 8 o jogador perde tudo.
+#"if" a soma der 3, 4, 9, 10 ou 11 ganha o mesmo que apostou mais o dobro.
+#"if" a soma der 12 ganha o mesmo mais o triplo.
+
+
+
+#Any Craps - Pode ser feita em qualquer fase do jogo.
+#"if" soma dos dados der 2, 3 ou 12 o jogador ganha 7 vezes o que apostou.
+#"if" qualquer outra soma, o jogador perde.
+
+
+#Twelve - Pode ser feita em qualquer fase do jogo.
+#"if" if soma dos dados derem 12, o jogador ganha 30 vezes o que apostou.
+#"if" qualquer outra soma, o jogador perde
