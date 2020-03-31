a835558104466c75675067063cd0362d3595edc0
matheusrb <matheusbarrosr@gmail.com>
2020-03-31 10:06:16 -0300

    Comentarios
    Algumas medidas que deverão ser implementadas no jogo

---
+++ Craps_games.py
@@ -0,0 +1,5 @@
+#O jogo consiste em apostar no resultado de um par de dados. 
+
+#As apostas serão sempre de números inteiros positivos de fichas, e o jogador começa com uma quantidade de fichas definida por você.
+
+#Rodadas sucessivas, o jogador decide apostar ou sair do jogo. Automaticamente sai se as fichas acabarem.
