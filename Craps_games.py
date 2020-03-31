c8567b125315e150358a8780e293ed907a7eaa31
matheusrb <matheusbarrosr@gmail.com>
2020-03-31 10:29:22 -0300

    Update comentarios

--- Craps_games.py
+++ Craps_games.py
@@ -3,3 +3,17 @@
 #As apostas serão sempre de números inteiros positivos de fichas, e o jogador começa com uma quantidade de fichas definida por você.
 
 #Rodadas sucessivas, o jogador decide apostar ou sair do jogo. Automaticamente sai se as fichas acabarem.
+
+#1 Rodada pode ter duas fases, "Come out" e pode passar para a fase de "Point".
+
+#Deixe sempre o jogador informado de que fase ele está. Mostre para o jogador as possibilidades de apostas e pergunte qual a opção e o valor que deseja apostar.
+#Ele pode fazer apostas de mais de um tipo por vez
+
+# O computador é quem realiza o lançamento de dois dados (6 lados) para o jogo.
+
+
+
+#Pass Line Bet - Na fase de 'Come Out', "if" a soma dos dados for 7 ou 11 o jogador ganha e as fichas apostadas sao dobradas.
+#"if" a soma for 2, 3 ou 12 (Craps) o jogador perde a aposta.
+#"if" a soma for 4, 5, 6, 8, 9 ou 10 o jogo muda para a fase de 'Point'.
+#A aposta continua valendo, o valor tirado se torna o 'Point', para ganhar a soma do novo lancamento deve ser igual ao do point.
